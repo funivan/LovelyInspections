602ae276a52d33f506205e6cae89e6ea0dc1b369
Ivan Scherbak <dev@funivan.com>
2017-10-27 16:36:30 +0300

    show message on click

--- src/com/funivan/phpstorm/lovelyPhpInspections/idea/LineMarker.kt
+++ src/com/funivan/phpstorm/lovelyPhpInspections/idea/LineMarker.kt
@@ -1,17 +1,20 @@
 package com.funivan.phpstorm.lovelyPhpInspections.idea
 
 import com.funivan.phpstorm.lovelyPhpInspections.random.RandomItem
+import com.intellij.codeInsight.daemon.GutterIconNavigationHandler
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.psi.PsiElement
 import com.intellij.util.Function
+import java.awt.event.MouseEvent
 import javax.swing.Icon
+import javax.swing.JOptionPane
 
 
 class LineMarker<T : PsiElement>(
         psi: T,
         icons: RandomItem<Icon>,
-        awesome: TooltipPhrase
+        private val awesome: TooltipPhrase
 ) : LineMarkerInfo<T>(
         psi,
         psi.textRange,
@@ -19,5 +22,22 @@ class LineMarker<T : PsiElement>(
         10,
         Function { awesome.toString() },
         null,
-        GutterIconRenderer.Alignment.RIGHT
-)
+        GutterIconRenderer.Alignment.LEFT
+) {
+
+    override fun getNavigationHandler(): GutterIconNavigationHandler<T> {
+        return object : GutterIconNavigationHandler<T> {
+            override fun navigate(mouseEvent: MouseEvent, p1: T) {
+                
+                JOptionPane.showMessageDialog(
+                        null,
+                        awesome.toString(),
+                        "Awesome",
+                        JOptionPane.DEFAULT_OPTION
+                )
+
+
+            }
+        }
+    }
+}
